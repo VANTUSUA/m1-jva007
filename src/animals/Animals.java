a1c674b9451a04f44ed2631e07d000ce6bf41876
Mykhaylo <mg19@i.ua>
2018-11-27 12:18:05 +0200

    Test1

---
+++ src/animals/Animals.java
@@ -0,0 +1,36 @@
+package animals;
+
+public class Animals {
+    String roar = "R-r-r-r-r";
+    private String name;
+    private double weight;
+
+    public String getRoar() {
+        return roar;
+    }
+
+    public void setRoar(String roar) {
+        this.roar = roar;
+    }
+
+    private String roar() {
+        return roar;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
--- src/animals/Demo.java
+++ src/animals/Demo.java
@@ -1,13 +1,14 @@
 package animals;
 
+import static java.lang.Thread.sleep;
+
 public class Demo {
     public static void main(String[] args) {
 
         Dog dog = new Dog();
-        Adress adress = new Adress();
-        dog.bark();
+        System.out.println( " New object Dog is created");
 
         System.out.println( " " + dog.bark()+"\n "+"Dog name is " + dog.name + " \n" +
-                "Dog adress is " + adress.getAdress());
+                "Dog adress is ");
     }
 }
